a4889584013a81cef0c47380f9c1df4feabd6643
Alexander-Hac <hac22567@spengergasse.at>
2022-10-12 16:24:18 +0200

    Change

---
+++ Properties/Lehrer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProperties
+{
+    class Lehrer
+    {
+        public string Vorname { get; set; } = "";
+
+        public string Zuname { get; set; } = default(string);
+
+        private decimal? bruttogehalt = default(decimal?);
+
+        public decimal? Bruttogehalt
+        {
+            get { return bruttogehalt; }
+            set { bruttogehalt = bruttogehalt ?? value; } 
+        }
+
+        public string Kuerzel
+        {
+            get { return Zuname?.ToUpper().Substring(0, 3) ?? "";}
+        }
+
+        public decimal? Nettogehalt
+        {
+            get { return Bruttogehalt * 0.8M ?? 0M; }
+        }
+
+
+    }
+}
+    
---
+++ Properties/Rechteck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProperties
+{
+    class Rechteck
+    {
+
+        public Rechteck()
+        {
+
+        }   
+
+        private int laenge, breite;
+
+
+        public int Laenge
+        {
+            get { return laenge; }
+            set { laenge = value >= 0 ? value : throw new ArgumentException("Ungültige Länge"); }
+        }
+
+        public int Breite
+        {
+            get { return breite; }
+            set { breite = value >= 0 ? value : throw new ArgumentException("Ungültige Länge"); }
+        }
+
+        public int Flaeche
+        {
+            get { return Laenge * Breite; }
+        }
+
+
+    }
+}
+    
