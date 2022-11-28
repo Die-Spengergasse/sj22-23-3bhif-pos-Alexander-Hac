5bbb87c69a09fd6f51f2da48fee0720665d25ae3
AlexanderH <hac22567@spengergasse.at>
2022-11-28 13:25:35 +0100

    s

---
+++ Vererbung/Klasse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung
+{
+    class Klasse : List<Schueler>
+    {
+        public Klasse(string id)
+        {
+            Id = id;
+        }
+        public string Id { get; }
+
+        public Lehrer Kv { get; set; }
+
+        
+        public new void Add (Schueler s)
+        {
+            if(!this.Contains(s))
+            {
+              List<Schueler> sl = this;
+              sl.Add(s);           
+            } else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+       
+        public override string ToString()
+        {
+            return $"Klasse {this.Id}, KV: {this.Kv.Zuname}, Schüleranzahl: {this.Count} ";
+        }
+    }
+}
